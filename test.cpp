71eb062c534890bcca7373e0bc8672926899eacb
zamyy78 <zamyy78@gmail.com>
2022-01-19 20:08:49 -0800

    Fixes #2

--- test.cpp
+++ test.cpp
@@ -1,4 +1,5 @@
 #include "c-echo.h"
+#include "c-count.h"
 
 #include "gtest/gtest.h"
 
@@ -12,19 +13,19 @@ TEST(EchoTest, EmptyString) {
 	EXPECT_EQ("", echo(1, test_val));
 }
 
-TEST(EchoTest, Space) {
-	char* test_val[2]; test_val[0] = "./c-echo"; test_val[1] = " "; 
-	EXPECT_EQ(" ", echo(2, test_val));
+TEST(CountTest, HelloWorld) {
+	std::string test_str = "hello world"; 
+	EXPECT_EQ(2, count(test_str));
 }
 
-TEST(EchoTest, Character) {
-	char* test_val[2]; test_val[0] = "./c-echo"; test_val[1] = "h"; 
-	EXPECT_EQ("h", echo(2, test_val));
+TEST(CountTest, EmptyString) {
+	std::string test_str = "";
+	EXPECT_EQ(0, count(test_str));
 }
 
-TEST(EchoTest, Number) {
-	char* test_val[2]; test_val[0] = "./c-echo"; test_val[1] = "8"; 
-	EXPECT_EQ("8", echo(2, test_val));
+TEST(CountTest, ManySpaces) {
+	std::string test_str = "   this   string has     weird   spacing"; 
+	EXPECT_EQ(5, count(test_str));
 }
 
 int main(int argc, char **argv) {
